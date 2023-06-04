f0314519d0c4933dc9a707be5f9b5d3ac9b683b7
Josh Gilman <iamjoshgilman@gmail.com>
2023-06-03 17:33:24 -0700

    Secured the passwords with encryption

--- code/gen_ad.ps1
+++ code/gen_ad.ps1
@@ -1,33 +1,41 @@
-param( [Parameter(Mandatory=$true)] $JSONfile )
+param (
+    [Parameter(Mandatory=$true)]
+    $JSONfile
+)
 
-function CreateADGroup(){
-    param( [Parameter(Mandatory=$true)] $groupObject )
+function CreateADGroup {
+    param (
+        [Parameter(Mandatory=$true)]
+        $groupObject
+    )
 
     $name = $groupObject.name
     New-ADGroup -name $name -GroupScope Global
 }
 
-function CreateADUser(){
-    param( [Parameter(Mandatory=$true)] $userObject )
-    
-    # Pull out the name from the JSON object
-    $name = $userObject.name
-    $password = $userObject.password
+function CreateADUser {
+    param (
+        [Parameter(Mandatory=$true)]
+        $userObject
+    )
 
-    # Generate a "first inititial, last name" structure for username
+    # Pull out the name and password from the JSON object
+    $name = $userObject.name
+    $encryptedPassword = $userObject.password | ConvertTo-SecureString
+
+    # Generate a "first initial, last name" structure for the username
     $firstname, $lastname = $name.split(" ")
     $username = ($firstname[0] + $lastname).ToLower()
     $SamAccountName = $username
     $principalname = $username
 
     # Actually create the AD user object
-    New-ADUser -Name "$name" -GivenName $firstname -Surname $lastname -SamAccountName $SamAccountName -UserPrincipalName $principalname@$Global:Domain -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -PassThru | Enable-ADAccount
+    New-ADUser -Name "$name" -GivenName $firstname -Surname $lastname -SamAccountName $SamAccountName -UserPrincipalName "$principalname@$Global:Domain" -AccountPassword $encryptedPassword -PassThru | Enable-ADAccount
 
     # Add the user to its appropriate group
-    foreach($group_name in $userObject.groups) {
-        
+    foreach ($group_name in $userObject.groups) {
         try {
-            Get-ADGroup -Identity "$group_name"
+            Get-ADGroup -Identity $group_name
             Add-ADGroupMember -Identity $group_name -Members $username
         }
         catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException] {
@@ -40,10 +48,10 @@ $json = Get-Content $JSONFile | ConvertFrom-Json
 
 $Global:Domain = $json.domain
 
-foreach ($group in $json.groups){
+foreach ($group in $json.groups) {
     CreateADGroup $group
 }
 
-foreach ($userObject in $json.users){
+foreach ($userObject in $json.users) {
     CreateADUser $userObject
 }
